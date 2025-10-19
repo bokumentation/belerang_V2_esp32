192abdbc5c7e5d28aa8cb780350b7449cc0c35f7
Ibe Ibrahim <ibraheli41@gmail.com>
2025-10-19 21:36:41 +0700

    add some dependencies

--- main/include/board_pins.h
+++ main/include/board_pins.h
@@ -3,40 +3,34 @@
 
 #include "sdkconfig.h"
 
-#define BOARD_DEVKITC_ESP32_V2
 // #define BOARD_DEVKITC_ESP32S3_V1
-// #define BOARD_SUPERMINI_ESP32C3_V1
-// #define BOARD_HELTEC_WIRELESS_STICK_LITE_ESP32S3_V3
+// #define BOARD_HELTEC_WSL_ESP32S3_V3
 
 #ifdef CONFIG_IDF_TARGET_ESP32
-    #if defined(BOARD_DEVKITC_ESP32_V2)
-        #define ANEMOMETER_ADC_PIN GPIO_NUM_32
+    #define ANEMOMETER_ADC_PIN GPIO_NUM_32
 
-        #define H2S_UART_PORT UART_NUM_1
-        #define H2S_RX_PIN GPIO_NUM_26
-        #define H2S_TX_PIN GPIO_NUM_27
-        #define H2S_LOG_TAG "TB600B_H2S"
+    #define H2S_UART_PORT UART_NUM_1
+    #define H2S_RX_PIN GPIO_NUM_26
+    #define H2S_TX_PIN GPIO_NUM_27
+    #define H2S_LOG_TAG "TB600B_H2S"
 
-        #define SO2_UART_PORT UART_NUM_2
-        #define SO2_RX_PIN GPIO_NUM_16
-        #define SO2_TX_PIN GPIO_NUM_17
-        #define SO2_LOG_TAG "TB600B_SO2"
-    #endif
+    #define SO2_UART_PORT UART_NUM_2
+    #define SO2_RX_PIN GPIO_NUM_16
+    #define SO2_TX_PIN GPIO_NUM_17
+    #define SO2_LOG_TAG "TB600B_SO2"
 
 #elif defined(CONFIG_IDF_TARGET_ESP32C3)
-    #if defined(BOARD_DEVKITC_ESP32_V2)
-        #define ANEMOMETER_ADC_PIN GPIO_NUM_3 // Need to be fix. I dont remember the pin
+    #define ANEMOMETER_ADC_PIN GPIO_NUM_3 // Need to be fix. I dont remember the pin
 
-        #define H2S_UART_PORT UART_NUM_0
-        #define H2S_RX_PIN GPIO_NUM_9
-        #define H2S_TX_PIN GPIO_NUM_10
-        #define H2S_LOG_TAG "TB600B_H2S"
+    #define H2S_UART_PORT UART_NUM_0
+    #define H2S_RX_PIN GPIO_NUM_9
+    #define H2S_TX_PIN GPIO_NUM_10
+    #define H2S_LOG_TAG "TB600B_H2S"
 
-        #define SO2_UART_PORT UART_NUM_1
-        #define SO2_RX_PIN GPIO_NUM_20
-        #define SO2_TX_PIN GPIO_NUM_21
-        #define SO2_LOG_TAG "TB600B_SO2"
-    #endif
+    #define SO2_UART_PORT UART_NUM_1
+    #define SO2_RX_PIN GPIO_NUM_20
+    #define SO2_TX_PIN GPIO_NUM_21
+    #define SO2_LOG_TAG "TB600B_SO2"
 
 #elif defined(CONFIG_IDF_TARGET_ESP32S3)
     // Not tested
@@ -54,7 +48,7 @@
         #define SO2_LOG_TAG "TB600B_SO2"
 
     // Not Tested
-    #elif defined(BOARD_HELTEC_WIRELESS_STICK_LITE_ESP32S3_V3)
+    #elif defined(BOARD_HELTEC_WSL_ESP32S3_V3)
         #define ANEMOMETER_ADC_PIN GPIO_NUM_3
 
         #define H2S_UART_PORT UART_NUM_0
