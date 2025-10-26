a3a39e11a2fdf202ed4608ea21de920794f90ee5
Ibe Ibrahim <ibraheli41@gmail.com>
2025-10-26 15:13:52 +0700

    syntax

--- main/main.cpp
+++ main/main.cpp
@@ -5,13 +5,15 @@
     Last Revision: 251019
     Using Microsoft's Style Guide
 
+    Update: 
+    - clang-format add new style
+
     To do:
     - Add arduino-esp32 as ESP-IDF Components. We need this for Heltec LORA library
     - Add Global Variable and Pass the Reference. So can pass it in LORA.
-    - Implement MQTT Client (SPI -> W5500)
     - Implement SPI SD CARD (SPI -> SD CARD Module)
-    - Implement Battery level indicator (Digital GPIO -> INA Sensor)
-    - Implement CAN BUS (SPI -> Logic Level Shifter + MCP2515 | TJA1050)
+    - Implement Battery level indicator (I2C -> INA Sensor)
+    - Implement MQTT Client (SPI -> W5500)
     - Implement Serial Monitor for parsing data on WEB APP (UART 0)
 */
 
@@ -29,8 +31,7 @@
 extern "C" void app_main(void)
 {
     anemometer_handle_t *wind_sensor = anemometer_init(ANEMOMETER_ADC_PIN);
-    if (wind_sensor == nullptr)
-    {
+    if (wind_sensor == nullptr) {
         ESP_LOGE("MAIN", "Anemometer Failed");
         return;
     }
@@ -41,8 +42,7 @@ extern "C" void app_main(void)
     tb600b_handle_t *so2_sensor =
         tb600b_init(SO2_UART_PORT, SO2_TX_PIN, SO2_RX_PIN, BAUD_RATE, SO2_LOG_TAG, TB600B_SENSOR_TYPE_SO2);
 
-    if (h2s_sensor == nullptr || so2_sensor == nullptr)
-    {
+    if (h2s_sensor == nullptr || so2_sensor == nullptr) {
         ESP_LOGE("MAIN", "One or both sensors failed to initialize.");
         return;
     }
@@ -52,8 +52,7 @@ extern "C" void app_main(void)
 
     vTaskDelay(pdMS_TO_TICKS(1000));
 
-    while (1)
-    {
+    while (1) {
         // 1. EXECUTE ALL MEASUREMENTS FIRST
         anemometer_measure_and_update(wind_sensor); // Updates every 10s non-blocking
         tb600b_measure_and_update(h2s_sensor);
@@ -63,11 +62,13 @@ extern "C" void app_main(void)
         // 2. AGGREGATE AND PRINT ALL DATA ONCE
         ESP_LOGI("MAIN", "--- Aggregating Cycle Data ---");
 
-        // This function gathers the latest data from all handles and prints ONE JSON line
+        // This function gathers the latest data from all handles and prints ONE
+        // JSON line
         print_all_data_json(wind_sensor, h2s_sensor, so2_sensor);
 
         // 3. WAIT FOR THE NEXT MEASUREMENT CYCLE
-        // Total delay is now 500ms + 4500ms = 5 seconds between SO2 read and H2S read
+        // Total delay is now 500ms + 4500ms = 5 seconds between SO2 read and H2S
+        // read
         vTaskDelay(pdMS_TO_TICKS(4500));
     }
 }
